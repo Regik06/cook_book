9edd85626b5e7dfad93ebf1af4b11c558a94577b
Regik_06 <eparis@atlassian.com>
2023-01-17 14:58:52 +0300

    change

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ with open('recept.txt', encoding='utf-8') as file:
     # res = file.readlines()
     # print(res)
     cook_book = {}
+
     for line in file:
-        file.seek(0)
-        dish = file.readline().strip()
+        # file.seek(0)
+        dish = line.strip()
         quantity_ingredients = int(file.readline())
         # print(dish)
         ingredients = []
@@ -17,7 +18,10 @@ with open('recept.txt', encoding='utf-8') as file:
                 'measure': measure
             })
         cook_book[dish] = ingredients
+        file.readline()
+print(cook_book)
 
-    print(cook_book)
+def get_shop_list_by_dishes(dishes, person_count):
+    
 
 
